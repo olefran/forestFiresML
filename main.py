ec07f6aa35cc04e4a3647417802ccd1f5c1d60df
Daniel Lopez A01121284 <78670798+lopezd16@users.noreply.github.com>
2024-11-16 12:49:45 -0600

    Main py used for FASAPI

--- main.py
+++ main.py
@@ -1,36 +1,60 @@
-import forestFiresML.feractoringClass as m
+# main.py
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from typing import List
+import pickle
+import numpy as np
+import pandas as pd
+from ucimlrepo import fetch_ucirepo
+import sys
 
-def main():
+class DataIngest():
+    @staticmethod
+    def data_ingest():
+        """
+        Perform data ingestion from remote source.
+        """
+        # 162 for forest fires
+        dataset_ = fetch_ucirepo(id=162)
+        X = dataset_.data.features
+        y = dataset_.data.targets
+        return pd.concat([X, y], axis = 1)
+    
+if __name__ == '__main__':
+    output_file = sys.argv[1]
+    data = DataIngest.data_ingest()
+    data.to_csv(output_file, index=False)
 
-    print("Initializing Data Download on ./data")
-    data_ = m.DataImport.data_fetch()
 
-    print("Displaying Histograms")
-    m.DataExplorer.plot_histograms(data_)
+# Load the model
+with open("model.pkl", "rb") as f:
+    model = pickle.load(f)
 
-    print("Preparing data...")
+# Load the target names (class labels)
+data = DataIngest()
+target_names = data.data_ingest
 
-    m.DataExplorer.data_exp_n_prep(data_)
+# Define the input data format for prediction
+class ForestFire(BaseModel):
+    features: List[float]
 
-    print("Information about data:")
+# Initialize FastAPI
+app = FastAPI()
 
-    m.DataExplorer.explore_data(data_)
+# Prediction endpoint
+@app.post("/predict")
+def predict(ff_data: ForestFire):
+    if len(ff_data.features) != model.n_features_in_:
+        raise HTTPException(
+            status_code=400,
+            detail=f"Input must contain {model.n_features_in_} features."
+        )
+    # Predict
+    prediction = model.predict([ff_data.features])[0]
+    prediction_name = target_names[prediction]
+    return {"prediction": int(prediction), "prediction_name": prediction_name}
 
-    print("Processing data...")
-
-    m.FF_model.data_processing(data_)
-
-    print("Model creation and test run:")
-
-    m.FF_model.model_creation(data_)
-
-    print("Ml_traking requires a MLServer instance running on http://localhost:5000")
-
-    m.FF_model.mlflow_tracking(data_)
-
-    print("Best model with: ")
-
-    m.FF_model.best_model_run(data_)
-
-if __name__ == "__main__":
-    main()
+# Root endpoint
+@app.get("/")
+def read_root():
+    return {"message": "Fire Forest model API"}
